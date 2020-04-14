3449fb1f8fb0db8a33a4e189fffbb8aeae5cf57c
Garrett Egan <22334769+garrettwegan@users.noreply.github.com>
2020-04-14 07:03:46 -0500

    Added cookies to persist selection

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const newMainTitle = 'Project complete';
 const newDescription = 'Only one more thing to do...';
 const newLinkDescription = 'Fill this in';
 const newLink = 'https://google.com';
+const cookieName = 'persistUrl';
+const cookieLifetimeDays = 90;
 
 /**
  * HTMLRewriter classes
@@ -46,13 +48,12 @@ const rewriter = new HTMLRewriter()
     .on('a#url', new ContentRewriter(newLinkDescription))
     .on('a#url', new AttributeRewriter('href', newLink));
 
-// Will hold array of URLs once it has been retrieved
+// Will hold array of URLs once they have been retrieved
 let urls = null;
 
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
-
 
 /**
  * Respond to GET request with one of the given URLs,
@@ -60,7 +61,45 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
+  const cookie = getCookieValue(request.headers.get('cookie'), cookieName);
+  const url = cookie ? cookie : await getRandomURL();
 
+  let response = await fetch(url);
+
+  // set the cookie if it did not exist
+  if(!cookie){
+    response = new Response(response.body, response);
+    const persistenceCookie = `${cookieName}=${url}; max-age=${cookieLifetimeDays * 24 * 60 * 60}; Path='/';`
+    response.headers.set('Set-Cookie', persistenceCookie);
+  }
+
+  return rewriter.transform(response);
+}
+
+/**
+ * Gets the value of the specified cookie
+ * @param cookiesText, a string of cookies
+ * @param target, the name of the cookie we are looking for
+ * @returns {string | null}, the value of the cookie
+ */
+function getCookieValue(cookiesText, target){
+  let cookieValue = null;
+  if(cookiesText){
+    let cookies = cookiesText.split(';');
+    cookies.forEach(cookie => {
+      if(cookie.split('=')[0].trim() === target){
+        cookieValue = cookie.split('=')[1];
+      }
+    });
+  }
+  return cookieValue;
+}
+
+/**
+ * Gets a random URL from the API
+ * @returns {Promise<Response>}
+ */
+async function getRandomURL(){
   // only make API call once, no need to retrieve
   // the URLs with every request
   if(!urls){
@@ -77,13 +116,10 @@ async function handleRequest(request) {
     }
   }
 
-  // randomly choose a URL and log the choice
-  // before modifying and returning it to the user
+  // randomly choose a URL, modify it, and return it to the user
   const index = Math.floor(Math.random() * urls.length);
   const url = urls[index];
   console.debug('Redirecting to variant: ' + (index + 1) + ' at url: ' + url);
-
-  const response = await fetch(url);
-
-  return rewriter.transform(response);
+  return url;
 }
+
